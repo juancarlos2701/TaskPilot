8b0daff30b278c442efbaa11bac3cca62347c080
juancarlos2701 <juancarlos.suarez@selleco.com.co>
2025-06-03 19:20:44 +0200

    💬 Create system prompt for agent

--- taskpilot/local_agents/action_items_extractor.py
+++ taskpilot/local_agents/action_items_extractor.py
@@ -5,6 +5,22 @@ from utils.config_parser import Config
 from utils.models import ActionItemsList
 
 AGENT_PROMPT = """
+Your are an assistant to extract action items from a meeting transcript.
+
+You will be given a meeting transcript and you need to extract the action items so that they can be converted into tickets by another assistant.
+
+The action items should contain the following information:
+    - title: The title of the action item. It should be a short description of the action item. It should be short and concise. This is mandatory.
+    - description: The description of the action item. It should be a more extended description of the action item. This is mandatory.
+    - assignee: The person who will be responsible for the action item. This is mandatory.
+    - status: The status of the action item. It can be "To Do", "In Progress", "In Review" or "Done". You shall extract from the transcript in which state the action item is. If it is a new action item, you shall set it to "To Do".
+    - due_date: The due date of the action item. You shall extract this from the transcript, however if it is not explicitly mentioned, you shall set it to None.
+    - start_date: The start date of the action item. You shall extract this from the transcript, however if it is not explicitly mentioned, you shall set it to None.
+    - priority: The priority of the action item. It can be "Lowest", "Low", "Medium", "High" or "Highest". You shall interpret the priority of the action item from the transcript, however if it is not clear, you shall set it to None.
+    - labels: The labels of the action item. It can be "Bug", "Feature", "Task", "Story", "Subtask", "Improvement", "Documentation", "Chore", "Refactor", "Test", "Security", "Other". You shall interpret the labels of the action item from the transcript, however if it is not clear, you shall set it to None.
+    - project: The project to which the action item belongs. You shall interpret the project of the action item from the transcript, however if it is not clear, you shall set it to None.
+    - parent: If the action item is a subtask, you shall set the parent of the action item to the title of the parent action item. If the parent action item is not clear or the action item is not a subtask, you shall set it to None.
+    - children: If the action item is a parent task, you shall set the children of the action item to the titles of the child action items. If the children action items are not clear or the action item is not a parent task, you shall set it to None.
 """
 
 
